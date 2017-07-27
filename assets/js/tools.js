2b1ccbdcb1de4f9f672a5c164febda10cdc9c251
Timur Ibatullin <timur@ibatullin.com>
2017-07-27 21:44:44 +0300

    Move theme files from engnr.io repository

---
+++ assets/js/tools.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('._fadeIn').fadeIn(1000);
+
+});
+
+
+$('.ui.embed').embed({
+  parameters: {
+    rel: 0,
+    showinfo: 0,
+    iv_load_policy: 3,
+    modestbranding: 0
+  }
+});
